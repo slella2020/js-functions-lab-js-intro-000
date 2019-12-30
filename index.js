51ea40bc6ac48cf1dc3972b9c1a2a8a82606be2d
Stephanie Lella <stelella2007@gmail.com>
2019-12-30 05:47:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,4 +2,3 @@
 function happyHolidays() { return "Happy holidays!"}
 function happyHolidaysTo (name) { return `Happy holidays, ${name}!`}
 function happyHolidaysTo (name, holiday) { return `Happy ${holiday}, ${name}!`}
-
