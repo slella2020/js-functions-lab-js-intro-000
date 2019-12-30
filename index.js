3c17c62e4084c8fceee38bc509080a8018074fe1
Stephanie Lella <stelella2007@gmail.com>
2019-12-30 05:52:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,4 +2,4 @@
 function happyHolidays() { return "Happy holidays!"}
 function happyHolidaysTo (name) { return `Happy holidays, ${name}!`}
 function happyHolidaysTo (name, holiday) { return `Happy ${holiday}, ${name}!`}
-function holidayCountdown (holiday, int) { return `It's ${int} days until ${holiday}!`}
+function holidayCountdown (holiday, days) { return `It's ${days} days until ${holiday}!`}
